a876077ac9712b67b45bb3333f92bc0355107143
Prism.RD <vnsctx666@gmail.com>
2026-04-27 17:03:12 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/dashdesk/',
+  base: '/', // 👉 就是这里：把 '/dashdesk/' 删掉，只留下 '/'
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
